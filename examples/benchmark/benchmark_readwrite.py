b3d5323c29e162d3d340eaf48580c48280399968
Jon Nordby <jononor@gmail.com>
2024-09-07 11:49:39 +0200

    benchmark: Skeleton script

---
+++ examples/benchmark/benchmark_readwrite.py
@@ -0,0 +1,28 @@
+
+import time
+import io
+
+import npyfile
+
+
+def main():
+
+    # do X repetitions of each
+    # measure total runtime
+    # compute kB/second
+    # print human formatted per test
+    # output a CSV with machine readable results at end
+
+    # write file to memory. io.BytesIO
+    # write file to disk. open
+
+    # read file from memory
+    # read file from disk
+
+
+    # run for different chunk sizes
+    # 1, 16, 256, 4096
+
+
+if __name__ == '__main__':
+    main()
